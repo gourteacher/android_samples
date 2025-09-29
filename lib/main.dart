96d606b94c7a7e71dfd9b533bd2a5e2a628df02a
gourteacher <ouaazs@algonquincollege.com>
2025-09-28 21:28:38 -0400

    Week 4 - Alert Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,96 +11,107 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Android Week 3',
+      title: 'Week 4 - Samples',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Week 3 - Using Scaffold'),
+      home: const MyHomePage(title: 'Week 4 - Alert Dialog'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void processOK(BuildContext context){
+    Navigator.of(context).pop();
+    var snackBar = SnackBar(content: Text('OK Clicked'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void processCancel(BuildContext context){
+    Navigator.of(context).pop();
+    var snackBar = SnackBar(content: Text('Cancel Clicked'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  AlertDialog displayDialog(BuildContext context) {
+
+    return AlertDialog(
+      title: const Text('AlertDialog'),
+      content: const Text('Press OK or Cancel to continue ...'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: ()  {
+            processCancel(context);
+          },
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            processOK(context);
+          },
+          child: Container(
+            color: Colors.blueAccent,
+            padding: const EdgeInsets.all(14),
+            child: const Text("OK"),
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        actions: [
-          OutlinedButton(onPressed: () { }, child:Text("Button 1")),
-          OutlinedButton(onPressed: (){ }, child: Text("Button 2"))
-        ],
       ),
-
-      drawer:Drawer(child:Text("Hi there")),
-
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem( icon: Icon(Icons.camera), label: 'Camera'  ),
-          BottomNavigationBarItem( icon: Icon(Icons.add_call), label: 'Phone' ),
-        ],
-      ),
-
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child:  Stack (
-          children: [
-            Image.asset("images/algonquin.jpg"),
-            Text("Algonquin College", style: TextStyle(fontSize: 30.0, backgroundColor:Colors.white ),)
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () =>
+                    showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => displayDialog(context)
+                    ),
+                child: Text ('Click Me',
+                  style: TextStyle(fontSize: 30, color: Colors.blueAccent),
+                ),
+              ),
+            ),
           ],
         ),
       ),
-
     );
   }
 }
